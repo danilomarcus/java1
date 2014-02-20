d4923032822d6b697b2f82a4d8aae1ff43135824
Danilo Marcus <danpayne16@msn.com>
2014-02-19 23:59:25 -0300

    Primeiro Programa

---
+++ PrimeiroPrograma.java
@@ -0,0 +1,9 @@
+/*
+* Danilo Marcus - Primeiro Programa
+* https://github.com/danilomarcus
+*/
+class PrimeiroPrograma{
+	public static void main(String[] args){
+		System.out.println("Meu Primeiro Programa em Java funcionou!");
+	}
+}
